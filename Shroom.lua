510acfea1b5453a85bf5f24d794bade5237843c7
Quoteme <Luca@qcoded.de>
2014-02-26 20:31:40 +0100

    Create Shroom.lua
    just a normal 1up-shroom from a popular sidescrooling game to eyecandy your terminal.

---
+++ Shroom.lua
@@ -0,0 +1,25 @@
+-->Made by Luca-Leon Happel
+--->Thanks to http://www.glassgiant.com/ for the providing of the ascii codes
+---->feel free to modifie, just remember to leve these three lines here^^
+
+
+print("\27[1;30m             MMMMMMMMMMMMM              ")
+print("\27[1;30m         MMMMMM\27[1;37m,,,\27[1;32mDDDD\27[1;37m,,\27[1;30mMMMMMMM         ")
+print("\27[1;30m       MMMM\27[1;37m,,,,,,,\27[1;32mDDDD\27[1;37m,,,,,,,\27[1;30mMMMM       ")
+print("\27[1;30m       MMMM\27[1;37m,,,,,,,\27[1;32mDDDD\27[1;37m,,,,,,,\27[1;30mMMMM       ")
+print("\27[1;30m    MMMMM\27[1;32mDD\27[1;37m,,,,\27[1;32mDDDDDDDDD\27[1;37m,,,,,\27[1;32mDD\27[1;30mMMMM     ")
+print("\27[1;30m    MMM\27[1;37m,,\27[1;32mDDDDDDDDDDDDDDDDDDDDDD\27[1;37m,,\27[1;30mMM     ")
+print("\27[1;30m  MMMMM\27[1;37m,,\27[1;32m==DDDD=\27[1;37m,,,,,,,,\27[1;32mDDDDZ\27[1;37m,,,,\27[1;30mMMMM   ")
+print("\27[1;30m  MMMMM\27[1;37m,,,,\27[1;32mDDDD=\27[1;37m,,,,,,,,\27[1;32mDDDDI\27[1;37m,,,,\27[1;30mMMMM   ")
+print("\27[1;30m  MM+\27[1;37m,,,,,,\27[1;32mDD\27[1;37m,,,,,,,,,,,,,\27[1;32mDDI\27[1;37m,,,,,,\27[1;30mMM   ")
+print("\27[1;30m  MM7\27[1;37m,,,,\27[1;32mDDDD\27[1;37m,,,,,,,,,,,,,\27[1;32mDDDDD\27[1;37m,,,,\27[1;30mMM   ")
+print("\27[1;30m  MM8\27[1;37m,,,,\27[1;32mDDDD\27[1;37m,,,,,,,,,,,,,\27[1;32mDDDDD\27[1;37m,,,,\27[1;30mMM   ")
+print("\27[1;30m  MMM\27[1;32mDDDDDDDDDDI\27[1;37m,,,,,,,,\27[1;32mDDDDDDDDDDD\27[1;30mMM   ")
+print("\27[1;30m  MMM\27[1;32mDDDD\27[1;30mMMMMMMMMMMMMMMMMMMMMMM\27[1;32mDDDD\27[1;30mMM   ")
+print("\27[1;30m  NMMMMMMMM\27[1;33m,,,,+\27[1;30mMM\27[1;33m,,,,\27[1;30mMM\27[1;33m,,,,\27[1;30mMMMMMMMMM   \27[1;30m")
+print("\27[1;30m  DMMMMMMMM\27[1;33m,,,,+\27[1;30mMM\27[1;33m,,,,\27[1;30mMM\27[1;33m,,,,\27[1;30mMMMMMMMMM   \27[1;30m")
+print("\27[1;30m     MMMM\27[1;33m,,,,,,,\27[1;30mMM\27[1;33m,,,,\27[1;30mMM\27[1;33m,,,,,,,\27[1;30mMMMM     \27[1;30m")
+print("\27[1;30m       MM\27[1;33m,,,,,,,,,,,,,,,,,,,,,,\27[1;30mMM       \27[1;30m")
+print("\27[1;30m       MMMM\27[1;33m,,,,,,,,,,,,,,,,,\27[1;30mMMMMM       \27[1;30m")
+print("\27[1;30m       MMMM\27[1;33m,,,,,,,,,,,,,,,,,\27[1;30mMMMMM       \27[1;30m")
+print("\27[1;30m         MMMMMMMMMMMMMMMMMMMMMM         \27[1;30m")
